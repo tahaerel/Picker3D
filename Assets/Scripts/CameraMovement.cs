a91df569105da9522af88b3556e2ede12c647c43
Taha Erel <tahaerelll@gmail.com>
2023-11-23 18:26:05 +0300

    Update Scripts
    Adding score counter, score check, move up skill to moving platform
    Adding dynamic text to moving platform
    Cam movement for stop or move player
    Scene changed

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,10 +2,41 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    private bool isCameraStop;
     private float cameraMoveSpeed = 4.5f;
+
+    private void Start()
+    {
+        MovingPlatform.containerStop += StopCameraMovement;
+        MovingPlatform.gatesUp += ContinueCameraMovement;
+    }
 
     private void LateUpdate()
     {
+        if (!isCameraStop)
+        {
+            MoveCamera();
+        }
+    }
+
+    private void MoveCamera()
+    {
         transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + cameraMoveSpeed * Time.deltaTime);
+    }
+
+    private void StopCameraMovement()
+    {
+        isCameraStop = true;
+    }
+
+    private void ContinueCameraMovement()
+    {
+        isCameraStop = false;
+    }
+
+    private void OnDestroy()
+    {
+        MovingPlatform.containerStop -= StopCameraMovement;
+        MovingPlatform.gatesUp -= ContinueCameraMovement;
     }
 }
--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -6,25 +6,25 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private Color containerPassColor;
-
-
-    private Vector3 targetPosition;
     private float containerUpSpeed = 5f;
+    private Vector3 targetPosition;
     private int sphereCount;
     private bool isUp;
+    private bool isTrigger;
+    private bool isLose;
 
     public static Action containerStop;
     public static Action containerPass;
     public static Action gatesUp;
 
     private float timer = 0.0f;
-
     private TextMeshPro textMesh;
     private int objectAmount;
 
     private void Start()
     {
         targetPosition = new Vector3(transform.position.x, -0.165f, transform.position.z);
+
         textMesh = GetComponentInChildren<TextMeshPro>();
         objectAmount = int.Parse(textMesh.text.Substring(3));
     }
@@ -46,33 +46,91 @@ public class MovingPlatform : MonoBehaviour
         if (transform.position.y >= -0.166f)
         {
             isUp = false;
-            if (containerPass != null)
-            {
-                containerPass();
-            }
+            ContainerPassAction();
         }
     }
 
+    private void ContainerPassAction()
+    {
+        containerPass?.Invoke();
+    }
+
     private void OnCollisionEnter(Collision collision)
+    {
+        HandleCollectableCollision(collision);
+        CheckAndMoveUp();
+    }
+
+    private void HandleCollectableCollision(Collision collision)
     {
         if (collision.transform.CompareTag("Collectable"))
         {
             sphereCount++;
             Destroy(collision.gameObject);
 
-            if (textMesh != null)
-                textMesh.text = sphereCount + " / " + objectAmount;
+            UpdateTextMesh();
         }
+    }
 
+    private void UpdateTextMesh()
+    {
+        if (textMesh != null)
+        {
+            textMesh.text = $"{sphereCount} / {objectAmount}";
+        }
+    }
+
+    private void CheckAndMoveUp()
+    {
         if (sphereCount >= objectAmount && timer >= 5f)
         {
             isUp = true;
             transform.GetComponent<Renderer>().material.color = containerPassColor;
 
             if (textMesh != null)
+            {
                 Destroy(textMesh, 0.5f);
+            }
 
             timer = 0f;
         }
+    }
+
+    private IEnumerator CheckIfLose()
+    {
+        yield return new WaitForSeconds(2f);
+
+        if (sphereCount < objectAmount)
+        {
+            //Level Fail UI
+            isLose = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HandlePlayerTrigger(other);
+    }
+
+    private void HandlePlayerTrigger(Collider other)
+    {
+        if (other.transform.CompareTag("Player") && !isTrigger)
+        {
+            containerStop?.Invoke();
+
+            if (!isLose)
+            {
+                StartCoroutine(CheckIfLose());
+            }
+
+            StartCoroutine(SetTriggerOnAndOff());
+        }
+    }
+
+    private IEnumerator SetTriggerOnAndOff()
+    {
+        isTrigger = true;
+        yield return new WaitForSeconds(5f);
+        isTrigger = false;
     }
 }
