9cc0fab85fe5bc9c3019aca27c9579affc5301da
Taha Erel <tahaerelll@gmail.com>
2023-11-23 13:45:15 +0300

    Added PlayerMovement script
    Added picker model
    Added Models, Materials, Prefabs folders
    Import TMP

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float xSpeed = 0.4f;
+    [SerializeField] private float moveForwardSpeed = 4.5f;
+
+    private float lastMousePoint;
+    private bool isMouseDown = false;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMouseDown = true;
+            lastMousePoint = Input.mousePosition.x;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isMouseDown = false;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        if (isMouseDown)
+        {
+            float difference = Input.mousePosition.x - lastMousePoint;
+            float xPos = Mathf.Clamp(transform.position.x + difference * Time.deltaTime * xSpeed, -1.4f, 1.4f);
+            rb.MovePosition(new Vector3(xPos, transform.position.y, transform.position.z + moveForwardSpeed * Time.fixedDeltaTime));
+            lastMousePoint = Input.mousePosition.x;
+        }
+        else
+        {
+            rb.MovePosition(new Vector3(transform.position.x, transform.position.y, transform.position.z + moveForwardSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
