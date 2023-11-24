0e4f9de0cf33a90b4a018b3a43c5ea6283eeeaba
Taha Erel <tahaerelll@gmail.com>
2023-11-24 12:56:38 +0300

    Add Gate System
    Add GateMovement
    Add gate prefab
    Add gate to game scene
    Change name plane to PlaneMaterial

---
+++ Assets/Scripts/GateMovement.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class GateMovement : MonoBehaviour
+{
+    private float gateSpeed = 70f;
+    private float signOfAngle;
+    private float angle;
+
+    private bool isGatesUp;
+    private bool isThisGate;
+
+    private static int isBothGatesUp;
+
+    void Start()
+    {
+        MovingPlatform.containerPass += LiftGates;
+
+        CalculateGateRotationAngle();
+    }
+
+    void Update()
+    {
+        OpenGates();
+
+        if(IsGatesFullyOpen())
+            GatesUp();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isThisGate = true;
+    }
+
+    private void CalculateGateRotationAngle()
+    {
+        signOfAngle = Mathf.Sign(transform.localPosition.x);
+
+        if (signOfAngle == 1)
+            angle = 60f;
+        else
+            angle = 300f;
+    }
+
+    private void OpenGates()
+    {
+        bool shouldOpen = isGatesUp && (transform.localRotation.eulerAngles.y * signOfAngle < angle * signOfAngle || transform.localRotation.eulerAngles.y == 0f);
+
+        if (shouldOpen)
+            transform.RotateAround(transform.parent.position, Vector3.forward, gateSpeed * signOfAngle * Time.deltaTime);
+    }
+
+    private void GatesUp()
+    {
+        if (IsGatesFullyOpen())
+        {
+            if (MovingPlatform.gatesUp != null)
+            {
+                isBothGatesUp++;
+
+                if(isBothGatesUp == 2)
+                {
+                    isBothGatesUp = 0;
+                    MovingPlatform.gatesUp();
+                }
+
+                isGatesUp = false;
+                isThisGate = false;
+            }
+        }
+    }
+
+    private bool IsGatesFullyOpen()
+    {
+        bool isFullyOpen = isGatesUp && transform.localRotation.eulerAngles.y * signOfAngle >= angle * signOfAngle;
+        return isFullyOpen;
+    }
+
+    private void LiftGates()
+    {
+        if(isThisGate)
+            isGatesUp = true;
+    }
+
+    private void OnDestroy()
+    {
+        MovingPlatform.containerPass -= LiftGates;
+    }
+}
